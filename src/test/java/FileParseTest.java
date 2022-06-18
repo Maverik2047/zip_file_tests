f0eb55d74b4450c777f6acd4aac156993d5491a1
Viacheslav <maverik2047@yandex.ru>
2022-06-18 15:16:29 +0300

    zip files test added.

--- src/test/java/FileParseTest.java
+++ src/test/java/FileParseTest.java
@@ -2,13 +2,14 @@ import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
 import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Test;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-import java.util.zip.ZipFile;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -17,7 +18,7 @@ public class FileParseTest {
 
     @Test
     void checkPDFTest() throws Exception {
-        ZipFile zipFile = new ZipFile(Objects.requireNonNull(classLoader.getResource("tests.zip")).getFile());
+        ZipFile zipFile = new ZipFile(Objects.requireNonNull(classLoader.getResource("test.zip")).getFile());
         ZipEntry entry = zipFile.getEntry("output.pdf");
         PDF pdf;
         InputStream inputStream = zipFile.getInputStream(entry);
@@ -28,7 +29,7 @@ public class FileParseTest {
 
     @Test
     void checkXLSTest() throws Exception {
-        ZipFile zipFile = new ZipFile(Objects.requireNonNull(classLoader.getResource("tests.zip")).getFile());
+        ZipFile zipFile = new ZipFile(Objects.requireNonNull(classLoader.getResource("test.zip")).getFile());
         ZipEntry entry = zipFile.getEntry("Sample.xlsx");
         XLS xls;
         InputStream inputStream = zipFile.getInputStream(entry);
@@ -36,20 +37,22 @@ public class FileParseTest {
         assertThat(xls.excel.getSheetAt(0).getRow(0).getCell(1).getStringCellValue())
                 .contains("test");
     }
+
     @Test
-    void checkCSVTest() throws Exception{
-        ZipFile zipFile = new ZipFile(Objects.requireNonNull(classLoader.getResource("tests.zip")).getFile());
+    void checkCSVTest() throws Exception {
+        ZipFile zipFile = new ZipFile(Objects.requireNonNull(classLoader.getResource("test.zip")).getFile());
         ZipEntry entry = zipFile.getEntry("file.csv");
         List<String[]> list;
-        InputStream inputStream =zipFile.getInputStream(entry);
+        InputStream inputStream = zipFile.getInputStream(entry);
         CSVReader csvReader = new CSVReader(new InputStreamReader(inputStream));
-        list=csvReader.readAll();
+        list = csvReader.readAll();
         assertThat(list).contains(new String[]
-                {"2", "isaac newton", "1643"});
+                {"2", "isaac newton", "1643" });
 
     }
+
     @Test
-    void jsonTest() throws Exception{
+    void jsonTest() throws Exception {
 
     }
 }
